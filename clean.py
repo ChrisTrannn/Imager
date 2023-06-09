7c898c2a081bde897bcfc6f5f961035d45908a95
Chris Tran <87344458+ChrisTrannn@users.noreply.github.com>
2023-06-09 09:53:28 -0700

    Create clean.py

---
+++ clean.py
@@ -0,0 +1,15 @@
+import os
+
+# Function to delete all files starting with "image" in current directory
+def delete_files_starting_with_image():
+    current_directory = os.getcwd()
+
+    for filename in os.listdir(current_directory):
+        if filename.startswith("image") or filename.startswith("video"):
+            file_path = os.path.join(current_directory, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+                print(f"Deleted file: {file_path}")
+
+# Call the function
+delete_files_starting_with_image()
